95a7b0464414e0619978164e9dfc853236bd83bd
luqman <luqman4719@gmail.com>
2023-04-04 17:23:23 +0300

    update index.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,37 @@
 </head>
 
 <body>
-
+    <div class="landing-page">
+        <div class="container">
+            <div class="header-area">
+                <div class="logo"></div>
+                <ul class="links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                <div class="login"><i></i><a href="">Sign In</a></div>
+                <div class="sign"><i></i><a href="">Sign Up</a></div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="banner-left">
+                <h1 class="title">Get Your Ticket Online, Easy and Safely</h1>
+                <a href="">Get Ticket Now</a>
+            </div>
+            <div class="banner-right">
+                <h4 class="title">Choose Your Ticket</h4>
+                <div class="tab-content">
+                    <form action="">
+                        <input type="text">
+                        <input type="text">
+                        <input type="text">
+                        <input type="submit" value="">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
