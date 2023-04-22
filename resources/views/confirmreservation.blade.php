83e425c23ed8d2f30f195e1c678f69c5abac59ff
luqman <luqman4719@gmail.com>
2023-04-22 23:01:32 +0300

    add confirm reservation

--- resources/views/confirmreservation.blade.php
+++ resources/views/confirmreservation.blade.php
@@ -18,7 +18,56 @@
 </head>
 
 <body>
-
+    <!-- Start Page Confirm -->
+    <div class="confirm">
+        <div class="container">
+            <div class="form-left">
+                <h1>Confirm Reservation</h1>
+                <div class="packge">
+                    <h3 class="name">First Name :</h3>
+                    <span class="">Luqman</span>
+                </div>
+                <div class="packge">
+                    <h3 class="name">Phone :</h3>
+                    <span class="">0932048737</span>
+                </div>
+                <div class="packge">
+                    <h3 class="name">Pickup Point :</h3>
+                    <span class="">Latakia</span>
+                </div>
+                <div class="packge">
+                    <h3 class="name">Dropping Point :</h3>
+                    <span class="">Damascus</span>
+                </div>
+                <div class="packge">
+                    <h3 class="name">Date :</h3>
+                    <span class="">4/4/2023</span>
+                </div>
+                <div class="packge">
+                    <h3 class="name">Start :</h3>
+                    <span class="">8 : 00</span>
+                </div>
+                <div class="packge">
+                    <h3 class="name">End :</h3>
+                    <span class="">9 : 00</span>
+                </div>
+                <div class="packge">
+                    <h3 class="name">Number bus :</h3>
+                    <span class="">1</span>
+                </div>
+                <div class="packge">
+                    <h3 class="name">Price :</h3>
+                    <span class="">1000</span>
+                </div>
+                <div class="packge">
+                    <h3>Number Of Seats</h3>
+                    <input type="number" name="" id="" min="1" max="5" value="1">
+                </div>
+                <button class="continue">Continue</button>
+            </div>
+        </div>
+    </div>
+    <!-- End Page Confirm -->
 </body>
 
 </html>
