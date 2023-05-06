8be99655ed26dacbf51eb4262e7907154c6584bb
luqman <luqman4719@gmail.com>
2023-05-06 21:34:21 +0300

    style pagination

--- resources/views/vendor/pagination/custom2.blade.php
+++ resources/views/vendor/pagination/custom2.blade.php
@@ -1,85 +1,83 @@
 <style>
-
     /* GENERAL STYLES */
-
-    .pagination{
-      padding: 30px 0;
+    :root {
+        --main-color: #0E9E4D;
+        --font-color: #424248;
+        --hover-color: #208f50;
     }
 
-    .pagination ul{
-      margin: 0;
-      padding: 0;
-      list-style-type: none;
+    .pagination {
+        padding: 30px 0;
+        text-align: center;
     }
 
-    .pagination ul li{
+    .pagination ul li {
         display: inline-block;
+        border: 0.5px solid var(--hover-color);
+        border-radius: 5px;
     }
-    .pagination a{
+
+    .pagination a {
         text-decoration: none;
         display: inline-block;
         padding: 10px 18px;
-        color: #122769;
+        color: black;
+        transition: 0.3s;
     }
 
-    .pagination ul li{
-      border: 0.5px solid #EA3509;
-    }
-    .pagination ul li:hover{
-        background-color: #EA3509;
+    .pagination ul li:hover a {
+        background-color: var(--hover-color);
         color: white;
+        cursor: pointer;
     }
-    .pagination ul li a{
-        /* color: #EA3509; */
-    }
-    .is-active, .disabled{
-        background-color: green;
+
+
+    .is-active,
+    .disabled {
+        background-color: var(--main-color);
         color: #ffff;
         font-weight: bold;
     }
+</style>
+@if ($paginator->hasPages())
+<div class="pagination">
+    <ul>
+        @if ($paginator->onFirstPage())
+        <li class="disabled">
+            <a href="#" tabindex="-1"><i class="fa-solid fa-arrow-left"></i></a>
+        </li>
+        @else
+        <li><a href="{{ $paginator->previousPageUrl() }}"><i class="fa-solid fa-arrow-left"></i></a></li>
+        @endif
 
+        @foreach ($elements as $element)
+        @if (is_string($element))
+        <li class="disabled">{{ $element }}</li>
+        @endif
+        @if (is_array($element))
+        @foreach ($element as $page => $url)
+        @if ($page == $paginator->currentPage())
+        <li class="is-active">
+            <a>{{ $page }}</a>
+        </li>
+        @else
+        <li>
+            <a href="{{ $url }}">{{ $page }}</a>
+        </li>
+        @endif
+        @endforeach
+        @endif
+        @endforeach
 
-
-    </style>
-    @if ($paginator->hasPages())
-    <div class="pagination">
-        <ul>
-            @if ($paginator->onFirstPage())
-                <li class="disabled">
-                    <a href="#" tabindex="-1">Previous</a>
-                </li>
-            @else
-                <li><a href="{{ $paginator->previousPageUrl() }}">Previous</a></li>
-            @endif
-
-            @foreach ($elements as $element)
-                @if (is_string($element))
-                    <li class="disabled">{{ $element }}</li>
-                @endif
-                @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                            <li class="is-active">
-                                <a>{{ $page }}</a>
-                            </li>
-                        @else
-                            <li>
-                                <a href="{{ $url }}">{{ $page }}</a>
-                            </li>
-                        @endif
-                    @endforeach
-                @endif
-            @endforeach
-
-            @if ($paginator->hasMorePages())
-                <li>
-                    <a href="{{ $paginator->nextPageUrl() }}" rel="next">Next</a>
-                </li>
-            @else
-                <li class="disabled">
-                    <a href="#">Next</a>
-                </li>
-            @endif
-        </ul>
-    </div>
-    @endif
+        @if ($paginator->hasMorePages())
+        <li>
+            <a href="{{ $paginator->nextPageUrl() }}" rel="next"><i class="fa-solid fa-arrow-right"></i></a>
+        </li>
+        @else
+        <li class="disabled">
+            <a href="#"><i class="fa-solid fa-arrow-right"></i></a>
+        </li>
+        @endif
+    </ul>
+</div>
+@endif
