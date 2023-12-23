d2a20e0fc58d0cb2d237abb7b7618f928448030a
AwsAboud <awsaboud7@gmail.com>
2023-12-23 17:48:28 +0300

    refactor filement recources

--- app/Filament/Widgets/UsersChart.php
+++ app/Filament/Widgets/UsersChart.php
@@ -10,66 +10,6 @@ class UsersChart extends LineChartWidget
 {
     protected static ?string $heading = 'Users Joined';
 
-    // protected function getData(): array
-    // {
-    //     $users = User::select('created_at')->get()->groupBy(function($users) {
-    //         return Carbon::parse($users->created_at)->format('F');
-    //     });
-    //     $quantities = [];
-    //     foreach ($users as $user => $value) {
-    //         array_push($quantities, $value->count());
-    //     }
-    //     return [
-    //         'datasets' =>[
-    //             [
-    //                 'label' => 'Users',
-    //                 'data' =>$quantities,
-    //                 'fill' => false,
-    //                 'borderColor' => 'rgb(75, 192, 192)',
-    //                 'tension' => 0.1
-    //             ],
-    //      ],
-    //      'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    //     ];
-    // }
-//     protected function getData(): array
-// {
-//     $users = User::select('created_at')->get()->groupBy(function($users) {
-//         return Carbon::parse($users->created_at)->format('F');
-//     });
-
-//     $quantities = [];
-//     $labels = [];
-
-//     // Loop through the months and add the user count to the quantities array
-//     // If there are no users for a particular month, add a zero to the quantities array
-//     foreach (range(1, 12) as $month) {
-//         $monthName = Carbon::createFromDate(null, $month, 1)->format('F');
-//         $label = $monthName;
-
-//         if (isset($users[$monthName])) {
-//             $count = $users[$monthName]->count();
-//             array_push($quantities, $count);
-//         } else {
-//             array_push($quantities, 0);
-//         }
-
-//         array_push($labels, $label);
-//     }
-
-//     return [
-//         'datasets' => [
-//             [
-//                 'label' => 'Users',
-//                 'data' => $quantities,
-//                 'fill' => false,
-//                 'borderColor' => 'rgb(75, 192, 192)',
-//                 'tension' => 0.1
-//             ]
-//         ],
-//         'labels' => $labels
-//     ];
-//  }
     protected function getData(): array
     {
         $users = User::select('created_at')->get()->groupBy(function($users) {
@@ -92,7 +32,6 @@ class UsersChart extends LineChartWidget
             if (isset($users[$monthName])) {
                 $count = $users[$monthName]->count();
                 $totalUser +=  $count;
-                //array_push($quantities, $count);
                 array_push($quantities, $totalUser);
             } else {
                 array_push($quantities, 0);
